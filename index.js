d26d747de9d5bf09f3abcd9b949a6f0c669d15c2
anushka pillay <93579943+anushkapillay@users.noreply.github.com>
2025-01-16 17:16:27 +0530

    index.js

--- index.js
+++ index.js
@@ -1,47 +1,86 @@
-let first_random_number = Math.floor(Math.random()*12)
-let second_random_number = Math.floor(Math.random()*12)
-
-let firstCard = first_random_number
-let secondCard = second_random_number
+let firstCard = getRamdonCard()
+let secondCard = getRamdonCard()
+let cards = [firstCard, secondCard]
 let sum = firstCard + secondCard
+let hasBlackJack = false
+let isAlive = false
+let message = ""
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
+let disable_start_btn = document.getElementById("disable-start-btn")
+let disable_btn = document.getElementById("disable-btn")
+let disable_replay_btn = document.getElementById("disable-replay-btn")
+let player = {
+    Name :"ANushka",
+    Chips :123
+ }
+let playerEl = document.getElementById("player-el")
+playerEl.textContent = player.Name +": "+player.Chips
 
-let game_state = false;
-let isAlive = true;
-
-let message = " "
-let messageEl = document.getElementById("text-message");
-
-let sumEl = document.getElementById("sum")
-let cardsEl = document.getElementById("selected-cards")
-let cards = [firstCard,secondCard];
 
 
+function getRamdonCard(){
+    let randomNumber = Math.floor(Math.random() * 13)+1
+    if(randomNumber>10){
+        return 10
+    }else if(randomNumber===1){
+        return 11
+    }else{
+        return randomNumber;
+    }
+    
+}
+function startGame() {
+    
+    isAlive = true;
+    let firstCard = getRamdonCard()
+    let secondCard = getRamdonCard()
+    cards[firstCard,secondCard]
+    sum = firstCard + secondCard;
+    renderGame()
+   
+}
 
-function startGame(){
+function renderGame() {
+    disable_btn.style.display = "block"
+     disable_start_btn.style.display = "none"
+    cardsEl.textContent = "Cards: "
+    // Create a for loop that renders out all the cards instead of just two
+    for (let i = 0; i < cards.length; i++) {
+        cardsEl.textContent += cards[i] + " "
+    }
+    
+    sumEl.textContent = "Sum: " + sum
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?"
+        isAlive = true
+    } else if (sum === 21) {
+        message = "You've got Blackjack! Refresh to Replay"
+        hasBlackJack = true
+        disable_btn.style.display = "none"
+
+    } else {
+        message = "You're out of the game! Refresh to restart"
+        isAlive = false
+    }
+    messageEl.textContent = message
+}
+function replay(){
     renderGame()
 }
-
-function renderGame(){
-    if(sum <= 20){
-        cardsEl.textContent = "Cards: " + cards[0] + "&" + cards[1]
-        sumEl.textContent = sum;
-        message = "Pick another card"
-     }else if(sum == 21){
-         message ="You got Black Jack"
-         game_state = true;
-     }else{
-         message = "You lost, Play again!"
-         isAlive = false
-     }
-messageEl.textContent = message     
+function newCard() {
+    console.log("new card drwan")
+    if (isAlive ==true && hasBlackJack ==false) {
+        let card = getRamdonCard()
+        sum += card
+        cards.push(card)
+        renderGame()        
+    }
+    if(!isAlive){
+        disable_btn.style.display = "none"
+    }
 }
 
-function newCard(){
-    console.log("new card drawn")
-    let new_card = firstCard;
-    sum += new_card;
-    console.log(sum)
-    renderGame()
-}
 
 
